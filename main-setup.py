2e2e934af3088e4334888e42fcd63ab2cd446322
Jonathan Ortiz <126116411+Furinkahzan@users.noreply.github.com>
2025-09-26 13:20:19 -0400

    Add files via upload

---
+++ main-setup.py
@@ -0,0 +1,34 @@
+def CPU:
+
+def CacheMemory:
+
+def PrimaryMemory:
+    Bool Power
+
+def SecondaryMemory:
+
+
+
+
+def main():
+    selectionMenu()
+
+
+def selectionMenu():
+    print("What would you like to simulate?")
+    print("1)Direct-Mapped")
+    print("2)Fully Associative")
+    print("3)LRU")
+    print("4)FIFO")
+    print("5)Set-Associative mapping")
+
+
+
+
+
+
+
+
+
+
+main()
