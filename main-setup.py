fc43a1a60e3268de28b6e1cf59b7190a035c60b4
Jonathan Ortiz <126116411+Furinkahzan@users.noreply.github.com>
2025-10-08 11:37:29 -0400

    Add files via upload

--- main-setup.py
+++ main-setup.py
@@ -1,5 +1,5 @@
 
-
+import collections
 # 4. Cache Access Logic
 # - Function to process one memory access:
 #     1. Decode address into tag and index
@@ -69,55 +69,27 @@ def adressAnalyze():{
 #     - tag
 #     - index
 #     - offset (optional, if modeling block size)
+   def __init__(self,tag:float, index:float, offset:float):
+        self.tag = tag
+        self.index = index
+        self.offset = offset
+
+    @property
+    def tag(self)-> float:
+        return self.tag
+            
+    @property
+    def index(self)-> float:
+        return self.tag    
+    
+    @property
+    def offset(self)-> float:
+        return self.tag       
 }
 
-# ============================
-# MAIN MENU
-# ============================
-# 1) Configure cache parameters
-#    - Change CacheSize, BlockSize, Associativity, etc.
-#    - Store updated values in SimConfig
+
 #
-# 2) Choose simulation type  (go to Simulation Type Submenu)
-#
-# 3) Load memory trace / access pattern
-#    - Choose synthetic (sequential, random, stride)
-#    - Or load from a trace file
-#
-# 4) Run simulation
-#    - Use current SimConfig + chosen simulation type + memory trace
-#    - Call the cache simulator logic
-#
-# 5) View statistics
-#    - Print results (total accesses, hits, misses, hit rate, miss rate)
-#    - Optionally save to file
-#
-# 6) Exit program
-#
-# ============================
-# SIMULATION TYPE SUBMENU
-# ============================
-# 1) Direct-Mapped
-#    - Sets Associativity = 1 automatically
-#
-# 2) Fully Associative
-#    - Sets Number of Sets = 1
-#    - Associativity = total blocks
-#
-# 3) Set-Associative
-#    - User chooses N-way (e.g., 2-way, 4-way)
-#
-# 4) LRU Policy
-#    - Set ReplacementPolicy = "LRU"
-#
-# 5) FIFO Policy
-#    - Set ReplacementPolicy = "FIFO"
-#
-# 6) Random Policy
-#    - Set ReplacementPolicy = "Random"
-#
-# 7) Back to Main Menu
-#
+
 # ============================
 # FLOW EXAMPLE
 # ============================
@@ -126,17 +98,35 @@ def adressAnalyze():{
 #        -> User selects "Direct-Map
 
 def main():
+    
     choice = selectionMenu()
     match choice:
         
-        case 1:
-        case 2:
-        case 3:
+        case 1:## 1) Configure cache parameters
+               #    - Change CacheSize, BlockSize, Associativity, etc.
+               #    - Store updated values in SimConfig
+            
+        case 2:# 2) Choose simulation type  (go to Simulation Type Submenu)
+            simulation_sub_menu()
+
+        case 3:# 3) Load memory trace / access pattern
+               #    - Choose synthetic (sequential, random, stride)
+               #    - Or load from a trace file
     
-        case 4:
-        case 5:
-        case 6:
+        case 4:# 4) Run simulation
+               #    - Use current SimConfig + chosen simulation type + memory trace
+               #    - Call the cache simulator logic
+               run_simulation()
+
+
+        case 5:# 5) View statistics
+               #    - Print results (total accesses, hits, misses, hit rate, miss rate)
+               #    - Optionally save to file
+
+
+        case 6:# 6) Exit program
             exit()
+
         case _:
             print("Invalid option")
     
@@ -144,25 +134,60 @@ def main():
 
 
 def selectionMenu():
-    
-    print("What would you like to simulate?")
-    print("1)Direct-Mapped")
-    print("2)Fully Associative")
-    print("3)LRU")
-    print("4)FIFO")
-    print("5)Set-Associative mapping")
+# ============================
+# MAIN MENU
+# ============================
+    print("What would you like to do?")
+    print("1)Configure cache parameters")
+    print("2)Choose simulation type")
+    print("3)Load memory trace")
+    print("4)Run simulation")
+    print("5)View statistics")
     print("6)exit")
     choice = input ("enter your choice:")
     return choice
 
+def run_simulation_menu():
+        choice = run_simulation_sub_menu()
+    match choice:
+        
+        case 1:## 1) Direct Mapped
+            
+        case 2:# 2)Fuly associative
+        case 3:# 3)set-associative
+    
+        case 4:# 4) LRU
 
+
+        case 5:# 5) FIFO
+
+        case 6:# 6) Random Policy
+          
+        case 7:# go back to main menu
+            
+        case _:
+            print("Invalid option")
+    
+def run_simulation_sub_menu():
+    # ============================
+# SIMULATION TYPE SUBMENU
+# ============================
+# 1) Direct-Mapped
+#    - Sets Associativity = 1 automatically
+#
+# 2) Fully Associative
+#    - Sets Number of Sets = 1
+#    - Associativity = total blocks
+    print("What type of simulation would you like to run?")
+    print("1)Direct mapped")
+    print("2)Fully associative")
+    print("3)Set-Assosiative")
+    print("4)LRU")
+    print("5)FIFO")
+    print("6)Random Policy")
+    print("7)Go back to the main menu")
+    choice= input("enter choice:")
+    return choice
 
 def simulator():
     
-
-
-
-
-
-
-main();
